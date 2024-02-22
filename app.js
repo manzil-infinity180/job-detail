b31c45d96a05be7f1c3e47ea1ae6b198cd450cb2
manzil-infinity180 <rahulvs2809@gmail.com>
2024-02-22 11:58:07 +0530

    fixing cors

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const jobDetailRouter = require("./router/jobDetailRouter");
-app.use(cors({
-  origin: ["https://frontend-anchors.onrender.com","*"],
-  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"], 
-  credentials:true
-}));
+// app.use(cors({
+//   origin: ["https://frontend-anchors.onrender.com","http://localhost:5173","*"],
+//   methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"], 
+//   credentials:true
+// }));
+let corsOptions = {
+  origin: function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins)
+    })
+  }
+}
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(bodyParser.json());
