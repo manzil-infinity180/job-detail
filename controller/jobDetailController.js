4f828cc7c65aa2aa7ce80cd06a0efc92c2355840
Rahul Vishwakarma <rahulvs2809@gmail.com>
2024-03-31 17:07:54 +0530

    minor changes into controller
    As we know we implemented the redis and when we take into our frontend it raise some conflict as we are sending the json data inside the details or user so i removed and make it as we get the data from redis 
    Will update this in good manner wait 🫤

--- controller/jobDetailController.js
+++ controller/jobDetailController.js
@@ -21,16 +21,13 @@ exports.getallJob = async(req,res,next)=>{
         res.status(200).json({
             length:detail.length,
             status:"Success",
-            data:{
               detail
-            }
+            
         });
     }catch(err){
         es.status(404).json({
             status:"Failed",
-            data:{
-              err:err.message
-            }
+            err:err.message
           })
     }
 }
@@ -54,16 +51,14 @@ exports.createJob = async(req,res,next)=>{
         const user = await Job.create(req.body);
         res.status(200).json({
             status:"Success",
-            data:{
               user
-            }
+            
         });
     }catch(err){
         res.status(404).json({
             status:"Failed",
-            data:{
               err:err.message
-            }
+           
           })
     }
 }
@@ -73,16 +68,14 @@ exports.findById = async(req,res,next)=>{
         const user = await Job.findById(id);
         res.status(200).json({
             status:"Success",
-            data:{
               user
-            }
+            
         });
     }catch(err){
         res.status(404).json({
             status:"Failed",
-            data:{
               err:err.message
-            }
+           
           })
     }
 }
@@ -109,16 +102,15 @@ exports.searchField = async(req,res,next)=>{
     console.log(results);
 
     res.status(200).json({
-      data : {results}
+        status:"success",
+     results
     })
 
   }catch(err){
     
     res.status(404).json({
       status:"Failed",
-      data:{
-        err:err.message
-      }
+      err:err.message
     })
 
   }
@@ -149,15 +141,13 @@ exports.autoComplete = async(req,res,next)=>{
     
     res.status(200).json({
       status:"success",
-      data : {results}
+      results
     })
 
   }catch(err){
     res.status(404).json({
       status:"Failed",
-      data:{
-        err:err.message
-      }
+      err:err.message
     })
   }
 }
